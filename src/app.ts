1ddef273b579c521053744dd6decf1b01acc4072
rehanp1 <rehanpathan8012@gmail.com>
2026-09-15 00:23:57 +0530

    feat: add authentication middleware and extend Express request types

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import walletRoutes from './modules/wallet/wallet.routes';
 import authRoutes from "./modules/auth/auth.routes"
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.get("/health", (_req, res) => {
    res.status(200).json({
