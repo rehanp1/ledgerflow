a70076ee092df918c099a17af98bcccc1e621117
Rehan Pathan <71642588+rehanp1@users.noreply.github.com>
2026-09-16 01:28:32 +0530

    Feat/auth module (#4)
    * feat: implement authentication module with user registration and login functionality
    
    * feat: add authentication middleware and extend Express request types
    
    * feat: implement authentication refresh and logout functionality with transaction support

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -71,3 +71,61 @@ export const login = async (req: Request, res: Response): Promise<void> => {
         res.status(500).json({ message: "Failed to login user"})
     }               
 }
+
+export const refresh = async (req: Request, res: Response): Promise<void> => {
+    try{
+        const refreshToken = req.cookies?.refresh_token;
+
+        if (!refreshToken) {
+            res.status(401).json({ message: "Refresh token required" });
+            return;
+        }
+
+        const result = await authService.refreshAccessToken(refreshToken);
+
+        res.cookie("access_token", result.accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "lax",
+            maxAge: 15 * 60 * 1000,
+        });
+
+        res.cookie("refresh_token", result.refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "lax",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+
+        res.status(200).json({ message: "Token refreshed" });
+
+    } catch (error) {
+        console.error("Refresh token error:", error);
+        res.status(500).json({ message: "Failed to refresh token" });
+    }
+}
+
+export const logout = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const refreshToken = req.cookies?.refresh_token;
+
+        await authService.logoutUser(refreshToken);
+
+        res.clearCookie("access_token", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "lax"
+        })
+
+        res.clearCookie("refresh_token", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "lax"
+        })
+
+        res.status(204).send();
+    } catch (error) {
+        console.error("Logout error:", error);
+        res.status(500).json({ message: "Failed to logout" });
+    }
+}
